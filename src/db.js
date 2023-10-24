f61a19275837b1c3724ccb52bd7f5fc94d2b8122
“Juan <ramirezjuanj0117@gmail.com>
2023-10-24 16:23:23 -0500

    test

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const conexionDB = async () => {
     try {
         await mongoose.connect( 'mongodb://127.0.0.1/empleadosList' )
-        console.log(">>> Base de datos conectada")
+        console.log(">>> Base de datos conectada <<<")
     } catch (error) {
         console.log(error)
         
