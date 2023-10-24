47a9945e79f36ac32f43e5db64f8b9e0ee20bec8
Juxnjo <ramirezjuanj0117@gmail.com>
2023-10-24 11:01:37 -0500

    db mongo connect

---
+++ src/db.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export const conexionDB = async () => {
+    try {
+        await mongoose.connect( 'mongodb://127.0.0.1/empleadosList' )
+        console.log(">>> Base de datos conectada")
+    } catch (error) {
+        console.log(error)
+        
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import app from "./app.js";
+import { conexionDB } from "./db.js";
 
+conexionDB()
 app.listen(8000)
 console.log("Server en el puerto", 8000)
