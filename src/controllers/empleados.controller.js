d2a9959e4d3a6d5db7b970655386f2929b0b134c
“Juan <ramirezjuanj0117@gmail.com>
2023-10-25 16:30:33 -0500

    getcontroller fix

--- src/controllers/empleados.controller.js
+++ src/controllers/empleados.controller.js
@@ -1,15 +1,19 @@
 import data from "../db.js";
 
-export const getEmpleados =  ( req, res ) =>
-{
-    const getData = "SELECT * FROM empleado"
-    data.query(getData, (error, results) => {
-        if (error) {
-            console.error('Error: ', error);
-            res.status(500).json({ error: 'Error:' });
+export const getEmpleados = ( req, res ) =>
+{   
+    const getData = 'SELECT * FROM empleado'
+    data.query( getData, ( err, rows ) =>
+    {
+        if ( err )
+        {
+            console.error( 'Error al realizar la consulta: ', err );
+            res.status( 500 ).json( { error: 'Error' } )
+            return;
         }
-        res.status(200).json(results)
-    })
-
+        res.status(200).json(rows)
+        console.log( 'Resultados: ', rows );
+    } );
 }
+
 
