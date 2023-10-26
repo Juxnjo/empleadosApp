74006844e4c0f585187ff6c68b4a52a936575a5e
Juxnjo <ramirezjuanj0117@gmail.com>
2023-10-26 17:44:22 -0500

    form fixes

--- client/src/pages/FormPage.jsx
+++ client/src/pages/FormPage.jsx
@@ -1,74 +1,56 @@
-//import { useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 function FormPage ()
 {
-    return (
-        <div className=" flex h-[calc(100vh-100px)] items-center justify-center">
-            <form action="" className="bg-purple-800 max-w-md w-full p-10 rounded-md">
-                <h1 className="text-xl font-semibold">Formulario</h1>
-                <br />
-                <div className="">
-                    <label htmlFor="">Nombre completo *</label>
-                    <input type="text" />
-                </div>
-                <div className="">
-                    <label htmlFor="">Correo electronico *</label>
-                    <input type="email" />
-                </div>
-                <div className="">
-                    <div className="">
-                        <label htmlFor="">Sexo *</label>
-                        <div className="">
-                            <input type="radio" />
-                            <label htmlFor="">Masculino</label>
-                        </div>
-                        <div>
-                            <input type="radio" />
-                            <label htmlFor="">Femenino</label>
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="">Area *</label>
-                        <select name="" id="">
-                            <option value="">1</option>
-                            <option value="">2</option>
-                            <option value="">3</option>
-                            <option value="">4</option>
-                            <option value="">5</option>
-                            <option value="">6</option>
-                        </select>
-                    </div>
-                    <div>
-                        <label htmlFor="">Descripcion *</label>
-                        <textarea type="text" name="" id="" />
-                    </div>
-                    <div>
-                        <input type="checkbox" />
-                        <label htmlFor="">Deseo recibir boletin informativo</label>
-                    </div>
-                    <div>
-                        <div>
-                            <label htmlFor="">Roles *</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" />
-                            <label htmlFor="">Dev</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" />
-                            <label htmlFor="">QA</label>
-                        </div>
-                        <div>
-                            <input type="checkbox" />
-                            <label htmlFor="">BA</label>
-                        </div>
 
-                    </div>
+    const { register, handleSubmit } = useForm()
+
+    return (
+        <div className="bg-zinc-800 max-w-md p-10 rounded-md">
+            <h1 className="text-xl font-semibold text-center my-2">Formulario</h1>
+            <form onSubmit={ handleSubmit( ( values ) =>
+            {
+                console.log( values )
+            } ) }>
+                <input type="text" { ...register( "nombre", { required: true } ) } className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Nombre completo *" />
+                <input type="email" { ...register( "email", { required: true } ) } className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Email *" />
+
+                <label htmlFor="">Sexo *</label>
+                <input type="radio" { ...register( "sexo", { required: true } ) } />
+                <label htmlFor="">Masculino</label>
+                <input type="radio" { ...register( "sexo", { required: true } ) } />
+                <label htmlFor="">Femenino</label>
+
+                <select name="" id="" { ...register( "area_id", { required: true } ) } className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" >
+                    <option defaultChecked></option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                </select>
+
+                <textarea type="text" name="" id="" { ...register( "descripcion", { required: true } ) } className="w-full bg-zinc-700 text-white px-4 py-2 rounded-md my-2" placeholder="Descripcion" />
+
+                <div className="my-2 ">
+                    <input type="checkbox" { ...register( "boletin", { required: false } ) } />
+                    <label htmlFor="" className="mx-2">Deseo recibir boletin informativo</label>
                 </div>
+                <div className="flex ">
+                    <label htmlFor="">Roles *</label>
+                    <input type="checkbox" { ...register( "rol_id", { required: true } ) } />
+                    <label htmlFor="">Dev</label>
+                    <input type="checkbox" { ...register( "rol_id", { required: true } ) } />
+                    <label htmlFor="">Dev</label>
+                    <input type="checkbox" { ...register( "rol_id", { required: true } ) } />
+                    <label htmlFor="">Dev</label>
+                </div>
+
                 <button type="submit">Guardar</button>
+
             </form>
 
         </div>
+
+
     )
 }
 
