a67585008455daf0e1daeaabe178ab6020c762b4
Juxnjo <ramirezjuanj0117@gmail.com>
2023-10-26 09:59:21 -0500

    estructura form OK

--- client/src/pages/FormPage.jsx
+++ client/src/pages/FormPage.jsx
@@ -7,19 +7,67 @@ function FormPage ()
             <form action="" className="bg-purple-800 max-w-md w-full p-10 rounded-md">
                 <h1 className="text-xl font-semibold">Formulario</h1>
                 <br />
-                <div className="space-x-10">
+                <div className="">
                     <label htmlFor="">Nombre completo *</label>
                     <input type="text" />
                 </div>
-                <div className="space-x-10 space-y-5">
+                <div className="">
                     <label htmlFor="">Correo electronico *</label>
                     <input type="email" />
                 </div>
-                <div className="space-x-10 space-y-5">
-                    <label htmlFor="">Sexo *</label>
-                    
+                <div className="">
+                    <div className="">
+                        <label htmlFor="">Sexo *</label>
+                        <div className="">
+                            <input type="radio" />
+                            <label htmlFor="">Masculino</label>
+                        </div>
+                        <div>
+                            <input type="radio" />
+                            <label htmlFor="">Femenino</label>
+                        </div>
+                    </div>
+                    <div>
+                        <label htmlFor="">Area *</label>
+                        <select name="" id="">
+                            <option value="">1</option>
+                            <option value="">2</option>
+                            <option value="">3</option>
+                            <option value="">4</option>
+                            <option value="">5</option>
+                            <option value="">6</option>
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="">Descripcion *</label>
+                        <textarea type="text" name="" id="" />
+                    </div>
+                    <div>
+                        <input type="checkbox" />
+                        <label htmlFor="">Deseo recibir boletin informativo</label>
+                    </div>
+                    <div>
+                        <div>
+                            <label htmlFor="">Roles *</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" />
+                            <label htmlFor="">Dev</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" />
+                            <label htmlFor="">QA</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" />
+                            <label htmlFor="">BA</label>
+                        </div>
+
+                    </div>
                 </div>
+                <button type="submit">Guardar</button>
             </form>
+
         </div>
     )
 }
